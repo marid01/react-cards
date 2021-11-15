d5d230d0949d798d622a264c2b63dd23b380c14d
dzimentey <dzimentey@gmail.com>
2021-11-15 18:01:58 +0300

    add: first sample of Reset Password page

--- src/main/ui/ResetPassword/ResetPassword.tsx
+++ src/main/ui/ResetPassword/ResetPassword.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
+import {SuperInputText} from "../common/SuperInputText/SuperInputText";
+import {SuperButton} from "../common/SuperButton/SuperButton";
+import { NavLink } from "react-router-dom";
+import {Path} from "../Routes/Routes";
 
 export const ResetPassword = () => {
-  return <div>Reset password</div>;
+
+    const [email, setEmail] = useState('')
+
+    const onEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
+
+    return <div>Reset password
+        <h3>It-incubator</h3>
+        <h2>Forgot your password?</h2>
+
+        <form>
+
+            <label>
+                <SuperInputText value={email} placeholder={'Email'} onChange={onEmailChange}/>
+                <div>Enter your email address and we will send you further instruction</div>
+            </label>
+
+            <SuperButton type={'submit'} style={{width: '200px', borderRadius: '30px'}}>Send instruction</SuperButton>
+        </form>
+
+        <p>Did you remember your password?</p>
+
+        <div>
+            <NavLink to = {Path.LogIn} activeClassName={''}>Try logging in</NavLink>
+        </div>
+
+    </div>;
 };
