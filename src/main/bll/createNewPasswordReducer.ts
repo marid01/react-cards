61eafb4379fd76f66b1985c7d241d941113fdb52
dzimentey <dzimentey@gmail.com>
2021-11-16 23:25:18 +0300

    add: fully working reducer

--- src/main/bll/createNewPasswordReducer.ts
+++ src/main/bll/createNewPasswordReducer.ts
@@ -1,13 +1,41 @@
+import {Dispatch} from "redux";
+import {cardsAPI} from "../dll/api";
+import {AxiosError} from "axios";
+
 type InitStateType = typeof initState;
 
-const initState = {};
+const initState = {
+  info: '',
+  error: '',
+};
 
-export const createNewPasswordReducer = (
-  state = initState,
-  action: any
-): InitStateType => {
-  switch (action.type) {
-    default:
-      return state;
-  }
+// reducer
+export const createNewPasswordReducer = (state = initState, action: ActionType): InitStateType => {
+    switch (action.type) {
+        case "CREATE-NEW-PASSWORD/SET-INFO":
+            return {...state, info: action.info};
+        case "CREATE-NEW-PASSWORD/SET-ERROR":
+            return {...state, error: action.error};
+        default:
+            return state;
+    }
+};
+
+// action
+type ActionType = ReturnType<typeof setNewPasswordInfoAC> | ReturnType<typeof setNewPasswordErrorAC>;
+
+export const setNewPasswordInfoAC = (info: string) => ({type: 'CREATE-NEW-PASSWORD/SET-INFO', info} as const);
+export const setNewPasswordErrorAC = (error: string) => ({type: 'CREATE-NEW-PASSWORD/SET-ERROR', error} as const);
+
+// thunk
+export const createNewPasswordTC = (password: string, resetPasswordToken: string) => (dispatch: Dispatch) => {
+    cardsAPI.setNewPassword(password, resetPasswordToken)
+        .then((res) => {
+            dispatch(setNewPasswordInfoAC(res.data.info));
+        })
+        .catch((error: AxiosError) => {
+            let errorMessage = error.response?.data.error
+            //console.log(errorMessage);
+            dispatch(setNewPasswordErrorAC(errorMessage));
+        })
 };
