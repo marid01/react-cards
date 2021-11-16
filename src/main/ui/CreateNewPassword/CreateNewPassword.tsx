4a5ae78f2571c6b7006dbe62073520fefee1df57
dzimentey <dzimentey@gmail.com>
2021-11-16 23:22:45 +0300

    add: Create New Password Page

--- src/main/ui/CreateNewPassword/CreateNewPassword.tsx
+++ src/main/ui/CreateNewPassword/CreateNewPassword.tsx
@@ -1,5 +1,55 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {SuperInputText} from "../common/SuperInputText/SuperInputText";
+import {SuperButton} from "../common/SuperButton/SuperButton";
+import {Redirect, useParams} from "react-router-dom";
+import {createNewPasswordTC, setNewPasswordErrorAC} from "../../bll/createNewPasswordReducer";
+import {RootStateType} from "../../bll/store";
 
 export const CreateNewPassword = () => {
-  return <div>Create new password</div>;
+
+  const info = useSelector<RootStateType, string>(state => state.createNewPassword.info)
+
+  const error = useSelector<RootStateType, string>(state => state.createNewPassword.error)
+
+  const dispatch = useDispatch()
+
+  let {token} = useParams<{token: string}>();
+
+  const [password, setPassword] = useState('')
+
+  const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+    dispatch(setNewPasswordErrorAC(''));
+  }
+
+  const onSubmitHandler = () => {
+    dispatch(createNewPasswordTC(password, token))
+  }
+
+  if (info !== '') {
+    alert(info)
+    return <Redirect to={'/login'} />
+  }
+
+  return <div>Create New Password Page
+
+    <h3>It-incubator</h3>
+    <h2>Create new password</h2>
+
+    <form onSubmit={onSubmitHandler}>
+
+      <label>
+        <SuperInputText value={password} placeholder={'New password'} onChange={onPasswordChange}/>
+        {error !== ''
+            ? <div style={{color: 'red', fontWeight: 500}}>{error}</div>
+            : <div>Enter your New Password and we will save it for you</div>
+        }
+      </label>
+
+      <SuperButton type={'submit'} style={{width: '200px', borderRadius: '30px'}}
+      >Create new password</SuperButton>
+    </form>
+
+  </div>;
 };
