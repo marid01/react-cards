2d00bfb7de1c9baf8aa394604b342415c1eb5380
dzimentey <dzimentey@gmail.com>
2021-11-16 23:19:19 +0300

    add: setNewPassword method; Url for Heroku

--- src/main/dll/api.ts
+++ src/main/dll/api.ts
@@ -1,24 +1,33 @@
 import axios, {AxiosResponse} from "axios";
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
     withCredentials: true,
 });
 
 export const cardsAPI = {
-    resetPassword(data: RequestResetPassType) {
-        return axios.post<RequestResetPassType, AxiosResponse<ResponseResetPassType>>('auth/forgot', data)
+    resetPassword(email: string, message: string) {
+        return instance.post<RequestResetPassType, AxiosResponse<ResponseResetPassType>>('auth/forgot',{email, message})
+    },
+    setNewPassword(password: string, resetPasswordToken: string) {
+      return  instance.post<RequestNewPasswordType, AxiosResponse<ResponseResetPassType>>('auth/set-new-password', {password, resetPasswordToken})
     },
 };
 
 export type RequestResetPassType = {
     email: string
-    from: string
+    //from: string
     message: string
-}
-
+};
 
 export type ResponseResetPassType = {
     info: string
-    error: string
+    //error: string
 };
+
+export type RequestNewPasswordType = {
+    password: string
+    resetPasswordToken: string
+};
+
+
