5a0cac32e964d84c46689414f2a3262cc0b9bbf5
dzimentey <dzimentey@gmail.com>
2021-11-22 23:47:44 +0300

    refactor: deny useLocation

--- src/a1-root/fogotPassword/CheckEmail.tsx
+++ src/a1-root/fogotPassword/CheckEmail.tsx
@@ -7,14 +7,15 @@ import style from "./ForgotPasswoed.module.scss";
 
 export const CheckEmail: React.FC = React.memo(() => {
 
-    const location = useLocation()
-// @ts-ignore
-    let mail = location.state.email
+    //  const location = useLocation<any>()
+    //
+    //
+    // let mail = location.state?.email
 
     return (
       <AuthModal subtitle='Check Email'>
           <div className={style.check}><EmailSvg/>
-              <div className={style.check__email}>{`We’ve sent an Email with instructions to ${mail}`}</div>
+              <div className={style.check__email}>{`We’ve sent an Email with instructions to your address`}</div>
           </div>
       </AuthModal>
 
